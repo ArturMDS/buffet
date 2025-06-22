1f17200c7b821fcf71613dc05c6a06aa0ff7b995
ArturMDS <arturmds@yahoo.com.br>
2025-06-22 18:00:15 -0300

    Configuração com Amazon S3 - 3

--- Buffet/settings.py
+++ Buffet/settings.py
@@ -91,11 +91,11 @@ if DB_NAME:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': env('DB_NAME'),
-            'USER': env('DB_USER'),
-            'PASSWORD': env('DB_PASSWORD'),
-            'HOST': env('DB_HOST'),
-            'PORT': env('DB_PORT', default='5432'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', default='5432'),
         }
     }
 else:
