dd5ca1cc0336c84a210cb294a1c53c2b1930ec40
ArturMDS <arturmds@yahoo.com.br>
2025-06-22 17:10:27 -0300

    Corrige configuração de arquivos estáticos

--- Buffet/settings.py
+++ Buffet/settings.py
@@ -143,15 +143,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR  / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
+    BASE_DIR / 'static',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
